3434723098116b5d34fa222ff9eb07b83eab5ea5
Gnani-is-Coding <gnanendragariminti@gmail.com>
2024-10-25 09:35:54 +0530

    adds video capturing ability

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 const captureBtn = document.querySelector(".capture-btn");
 const recBtn = document.querySelector(".record-btn");
+const videoElement = document.querySelector(".video")
+
+const constraints = {
+    audio: false,
+    video: true
+}
+let chunks = []
+
+let isRecording = false
+
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    videoElement.srcObject = stream
+    const mediaRecorder = new MediaRecorder(stream)
+
+    mediaRecorder.addEventListener("start", () => {
+        chunks = []
+    })
+
+    mediaRecorder.addEventListener("dataavailable",(e) => {
+        chunks.push(e.data)
+    })
+
+    mediaRecorder.addEventListener("stop", () => {
+        console.log("recording stopped")
+        const blob  = new Blob(chunks, {type: "video/mp4"})
+        chunks = []
+        const url =  URL.createObjectURL(blob)
+
+        const anchorEle = document.createElement("a")
+        anchorEle.href = url
+        anchorEle.download = "recording.mp4"
+        anchorEle.click()
+    })
+
+    recBtn.addEventListener("click", () => {
+        if (isRecording) {
+            mediaRecorder.stop()
+        } else {
+            mediaRecorder.start()
+        }
+        recBtn.classList.toggle("animate-recording")
+        isRecording = !isRecording
+        
+    })
+
+
+
+}).catch((err) =>
+    console.log(`error while recording: ${err.message}`)
+)
 
 captureBtn.addEventListener("click" , ()=> {
     captureBtn.classList.add("animate-capture")
@@ -9,6 +59,3 @@ captureBtn.addEventListener("click" , ()=> {
     }, 200)
 })
 
-recBtn.addEventListener("click", () => {
-    recBtn.classList.toggle("animate-recording")
-}) 
